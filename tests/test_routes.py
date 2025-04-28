62be0bac6fa28f7a3337192e3f9db16598783b73
ggcarv <ggc5@cin.ufpe.br>
2025-04-28 04:34:04 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,6 +13,7 @@ from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
 from service import talisman
+from flask import abort, request, jsonify
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
